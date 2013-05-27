6ab102ecf848bcc3b4a24cff7146035690148d6a
Ben Goodrich <bg2382@columbia.edu>
2013-05-27 12:30:42 -0400

    add line to make dependencies

--- hotfoot/test.sh
+++ hotfoot/test.sh
@@ -3,6 +3,7 @@ cd ${STAN_HOME}
 LINE=`expr ${PBS_ARRAYID} + 1`
 TEST=`sed -ne ${LINE}p ${OUTPUT}/tests.txt`
 OUT=${OUTPUT}/${PBS_JOBNAME}
+make CC=clang++ ${TEST}.d
 /usr/bin/time --output=${OUTPUT}/test_timings.txt --append -f "${TEST}: %E" \
 make CC="${CC}" ${TEST}
 exit 0
