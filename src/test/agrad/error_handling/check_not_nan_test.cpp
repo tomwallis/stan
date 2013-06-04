e32854af02544b4d08923cbfaac61f37d71ad4b1
Daniel Lee <bearlee@alum.mit.edu>
2013-06-04 12:02:38 -0400

    adding a fwd error_handling test for check_not_nan

--- src/test/agrad/error_handling/check_not_nan_test.cpp
+++ src/test/agrad/error_handling/check_not_nan_test.cpp
@@ -2,7 +2,7 @@
 #include <stan/agrad.hpp>
 #include <gtest/gtest.h>
 
-TEST(AgradErrorHandling,CheckNotNan) {
+TEST(AgradErrorHandling,CheckNotNan_var) {
   using stan::agrad::var;
   using stan::math::check_not_nan;
   const char* function = "check_not_nan(%1%)";
@@ -30,6 +30,41 @@ TEST(AgradErrorHandling,CheckNotNan) {
     << "check_not_nan should be true with x = -Inf: " << x_d;
 
   x = std::numeric_limits<var>::quiet_NaN();
+  x_d = std::numeric_limits<double>::quiet_NaN();
+  EXPECT_THROW(check_not_nan(function, x, "x", &result), std::domain_error)
+    << "check_not_nan should throw exception on NaN: " << x;
+  EXPECT_THROW(check_not_nan(function, x_d, "x", &result), std::domain_error)
+    << "check_not_nan should throw exception on NaN: " << x_d;
+}
+
+TEST(AgradErrorHandling,CheckNotNan_fvar) {
+  using stan::agrad::fvar;
+  using stan::math::check_not_nan;
+  const char* function = "check_not_nan(%1%)";
+  fvar<double> x(0.0, 0.0);
+  double x_d = 0;
+  fvar<double> result = 0;
+ 
+  EXPECT_TRUE(check_not_nan(function, x, "x", &result))
+    << "check_not_nan should be true with finite x: " << x;
+  EXPECT_TRUE(check_not_nan(function, x_d, "x", &result))
+    << "check_not_nan should be true with finite x: " << x_d;
+  
+  x = std::numeric_limits<fvar<double> >::infinity();
+  x_d = std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_not_nan(function, x, "x", &result))
+    << "check_not_nan should be true with x = Inf: " << x;
+  EXPECT_TRUE(check_not_nan(function, x_d, "x", &result))
+    << "check_not_nan should be true with x = Inf: " << x_d;
+
+  x = -std::numeric_limits<fvar<double> >::infinity();
+  x_d = -std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_not_nan(function, x, "x", &result))
+    << "check_not_nan should be true with x = -Inf: " << x;
+  EXPECT_TRUE(check_not_nan(function, x_d, "x", &result))
+    << "check_not_nan should be true with x = -Inf: " << x_d;
+
+  x = std::numeric_limits<fvar<double> >::quiet_NaN();
   x_d = std::numeric_limits<double>::quiet_NaN();
   EXPECT_THROW(check_not_nan(function, x, "x", &result), std::domain_error)
     << "check_not_nan should throw exception on NaN: " << x;
